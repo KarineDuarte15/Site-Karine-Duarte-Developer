06a7c8240a224e109277d02e68aaa3fb0f82df9c
KarinDuarte15 <karinegaebrasil@gmail.com>
2025-10-22 17:34:51 -0300

    feat: configurar turbopack no arquivo de configuração e remover componente SpeedInsights da página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,11 @@ import Blog from "@/components/Blog";
 import Contact from "@/components/Contact";   
 import Footer from "@/components/Footer";
 import EventsGallery from "@/components/EventsGallery";
-import { SpeedInsights } from "@vercel/speed-insights/next";  
+
 
 export default function Home() {
   return (
     <main>
-      <SpeedInsights />
       <Header />
       <Hero />
       <About />
